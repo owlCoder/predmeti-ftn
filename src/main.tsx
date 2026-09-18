e4cda18188373b7909b1867e7ba9e843f87920c3
Danijel Jovanović <danijel.xda@gmail.com>
2026-09-18 14:47:13 +0200

    Add ERS examples tab enhancer

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import StaticApp from './StaticApp'
+import ExamplesEnhancer from './ExamplesEnhancer'
 import './ui-refresh.css'
 
 document.documentElement.lang = 'sr'
@@ -9,5 +10,6 @@ document.body.style.margin = '0'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StaticApp />
+    <ExamplesEnhancer />
   </React.StrictMode>,
 )
